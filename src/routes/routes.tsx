ca66ee4f11f16d0bceedae8bc86773402a2f6a61
Simone Rosso <simone.rosso@concatel.com>
2020-10-22 23:00:24 +0200

    pagenotfopund

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense, lazy } from "react";
 import history from "../utils/history";
-import { Route, Switch, Redirect, Router } from "react-router-dom";
+import { Route, Switch, Router } from "react-router-dom";
 import { EuiLoadingChart } from "@elastic/eui";
 
 //lazy dynamic import of components
@@ -14,11 +14,10 @@ const RoutesProvider = () => (
   <Router history={history}>
     <Suspense fallback={<EuiLoadingChart size="m" />}>
       <Switch>
-        <Route exact path="/home" component={Homepage} />
+        <Route exact path="/" component={Homepage} />
         <Route exact path="/city" component={City} />
         <Route exact path="/province" component={Province} />
         <Route exact path="/province-list" component={ProvinceList} />
-        <Route exact path="/" render={() => <Redirect to="/home" />} />
         <Route path="*" component={PageNotFound} />
       </Switch>
     </Suspense>
--- src/views/PageNotFound/PageNotFound.tsx
+++ src/views/PageNotFound/PageNotFound.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "./PageNotFound.css";
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="page-not-found-container">
+      <h2>PageNotFound!</h2>
+      <img
+        src="https://media.giphy.com/media/FbfNWx3LPoy2I/giphy.gif"
+        alt="this slowpoke moves"
+        width="250"
+      />
+      <p>
+        <a href="/" className="link">
+          Go back to the Homepage
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default PageNotFound;
